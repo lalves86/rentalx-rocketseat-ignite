642f16cb8c7704b46da004c2d945a66ce499fa4b
lalves86 <lucas.alves@engenharia.ufjf.br>
2021-03-21 11:29:01 -0300

    Create the CategoryRepository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,13 @@
 import { Router } from "express";
 
-import { Category } from "../models/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRouter = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRouter.post("/", (request, response) => {
   const { name, description } = request.body;
-
-  const category = new Category();
-
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
-
-  categories.push(category);
-
+  categoriesRepository.create({ name, description });
   return response.status(201).send();
 });
 
