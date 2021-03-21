8174649f60d658c5da2c88540500998edb0c235a
lalves86 <lucas.alves@engenharia.ufjf.br>
2021-03-21 10:55:16 -0300

    Generate a random uuid for the category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,20 @@
 import { Router } from "express";
+import { v4 as uuidV4 } from "uuid";
 
 const categoriesRouter = Router();
 
 const categories = [];
 
-categoriesRouter.post("/categories", (request, response) => {
+categoriesRouter.post("/", (request, response) => {
   const { name, description } = request.body;
-  categories.push({
+
+  const category = {
+    id: uuidV4(),
     name,
     description,
-  });
+  };
+
+  categories.push(category);
 
   return response.status(201).send();
 });
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ import { categoriesRouter } from "./routes/categories.routes";
 const app = express();
 app.use(express.json());
 
-app.use(categoriesRouter);
+app.use("/categories", categoriesRouter);
 
 app.listen(3333, () => console.log("Server is running!"));
