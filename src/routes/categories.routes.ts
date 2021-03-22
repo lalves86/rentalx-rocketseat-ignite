e9de9feb36dfcfae3e64da4437d787643136bf0a
lalves86 <lucas.alves@engenharia.ufjf.br>
2021-03-22 06:43:39 -0300

    create category service

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,14 +1,21 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRouter = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRouter.post("/", (request, response) => {
   const { name, description } = request.body;
-  categoriesRepository.create({ name, description });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
   return response.status(201).send();
+});
+
+categoriesRouter.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+  return response.json(all);
 });
 
 export { categoriesRouter };
