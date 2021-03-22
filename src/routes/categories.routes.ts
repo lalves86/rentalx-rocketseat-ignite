3362144a21b96ca136ec7e317ad2c12acc88d24f
lalves86 <lucas.alves@engenharia.ufjf.br>
2021-03-22 20:35:40 -0300

    finish specification creation route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -3,19 +3,19 @@ import { Router } from "express";
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
 
-const categoriesRouter = Router();
+const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
-categoriesRouter.post("/", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
   const createCategoryService = new CreateCategoryService(categoriesRepository);
   createCategoryService.execute({ name, description });
   return response.status(201).send();
 });
 
-categoriesRouter.get("/", (request, response) => {
+categoriesRoutes.get("/", (request, response) => {
   const all = categoriesRepository.list();
   return response.json(all);
 });
 
-export { categoriesRouter };
+export { categoriesRoutes };
