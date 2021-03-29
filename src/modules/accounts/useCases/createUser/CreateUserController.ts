faa277fa39bb1d4301a785bca947432cf00c8e36
lalves86 <lucas.alves@engenharia.ufjf.br>
2021-03-29 07:55:23 -0300

    Create user structure

---
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+class CreateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, email, username, password, driver_license } = request.body;
+    const createUserController = container.resolve(CreateUserUseCase);
+    await createUserController.execute({
+      name,
+      email,
+      username,
+      password,
+      driver_license,
+    });
+    return response.status(201).send();
+  }
+}
+
+export { CreateUserController };
