8b8d149e9874e5345e294b2d7b4b1e043e1c6b10
lalves86 <lucas.alves@engenharia.ufjf.br>
2021-03-31 09:24:31 -0300

    Create test for CreateCategoryUseCase

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,47 @@
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+describe("Create category", () => {
+  let createCategoryUseCase: CreateCategoryUseCase;
+  let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory
+    );
+  });
+
+  it("Should be able to create a category", async () => {
+    const category = {
+      name: "Category Test",
+      description: "Category description test",
+    };
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    });
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name
+    );
+    expect(categoryCreated).toHaveProperty("id");
+  });
+
+  it("Should not be able to create two categories with the same name", async () => {
+    expect(async () => {
+      const category = {
+        name: "Category Test",
+        description: "Category description test",
+      };
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      });
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
