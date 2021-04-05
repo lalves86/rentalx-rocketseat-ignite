9bcb9a2954a17345ecc24d99da974d37f63d8239
lalves86 <lucas.alves@engenharia.ufjf.br>
2021-04-05 08:50:34 -0300

    Ensure a new rental can be created

---
+++ src/modules/rentals/useCases/CreateRentalUseCase.spec.ts
@@ -0,0 +1,23 @@
+import { RentalsRepositoryInMemory } from "../repositories/in-memory/RentalsRepositoryInMemory";
+import { CreateRentalUseCase } from "./CreateRentalUseCase";
+
+let createRentalUseCase: CreateRentalUseCase;
+let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+
+describe("Create rental", () => {
+  beforeEach(() => {
+    rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+    createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
+  });
+
+  it("Should be able to create a new rental", async () => {
+    const rental = await createRentalUseCase.execute({
+      user_id: "12345",
+      car_id: "121212",
+      expected_return_date: new Date(),
+    });
+
+    expect(rental).toHaveProperty("id");
+    expect(rental).toHaveProperty("start_date");
+  });
+});
