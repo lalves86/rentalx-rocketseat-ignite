d594d4a52322eafeea2bc01cd7ec5fbf702c4e5e
lalves86 <lucas.alves@engenharia.ufjf.br>
2021-04-09 13:35:48 -0300

    Create reset password structure

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
+import { ResetPasswordController } from "@modules/accounts/useCases/resetPasswordUser/ResetPasswordController";
 import { SendForgotPasswordEmailController } from "@modules/accounts/useCases/sendForgotPasswordEmail/SendForgotPasswordEmailController";
 
 const passwordRoutes = Router();
 
 const sendForgotPasswordEmailController = new SendForgotPasswordEmailController();
+const resetPasswordController = new ResetPasswordController();
 
 passwordRoutes.post("/forgot", sendForgotPasswordEmailController.handle);
+passwordRoutes.post("/reset", resetPasswordController.handle);
 
 export { passwordRoutes };
