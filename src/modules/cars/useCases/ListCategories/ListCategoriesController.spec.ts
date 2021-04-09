8d1252e46c0b864148ad2ace33111233521b68b3
lalves86 <lucas.alves@engenharia.ufjf.br>
2021-04-09 13:56:31 -0300

    Refactor tests to validate refresh token

--- src/modules/cars/useCases/ListCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/ListCategories/ListCategoriesController.spec.ts
@@ -32,7 +32,7 @@ describe("Create category controller", () => {
       password: "admin",
     });
 
-    const { token } = responseToken.body;
+    const { refresh_token } = responseToken.body;
 
     await request(app)
       .post("/categories")
@@ -41,7 +41,7 @@ describe("Create category controller", () => {
         description: "Category supertest description",
       })
       .set({
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${refresh_token}`,
       });
 
     const response = await request(app).get("/categories");
