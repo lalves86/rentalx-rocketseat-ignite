add189e9679e586060b4db5a62c5fa63d0a83ae8
lalves86 <lucas.alves@engenharia.ufjf.br>
2021-04-12 10:45:12 -0300

    Create interface for local storage provider

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   secret_token: "generateasecretkey",
-  expires_in_token: "15m",
+  expires_in_token: "30s",
   secret_refresh_token: "generateasecretrefreshtoken",
   expires_in_refresh_token: "30d",
   expires_refresh_token_days: 30,
--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -15,24 +15,13 @@ export async function ensureAuthenticated(
   next: NextFunction
 ): Promise<void> {
   const authHeader = request.headers.authorization;
-  const userTokensRepository = new UsersTokensRepository();
   if (!authHeader) {
     throw new AppError("Token missing", 401);
   }
   const [, token] = authHeader.split(" ");
   try {
-    const { sub: user_id } = verify(
-      token,
-      auth.secret_refresh_token
-    ) as IPayload;
+    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
 
-    const user = await userTokensRepository.findByUserIdAndRefreshToken(
-      user_id,
-      token
-    );
-    if (!user) {
-      throw new AppError("User does not exist", 401);
-    }
     request.user = {
       id: user_id,
     };
