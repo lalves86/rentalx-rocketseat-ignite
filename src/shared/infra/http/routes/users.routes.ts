b33fb0b958f21c44aa41ad3e753ed48d37dfa7fa
lalves86 <lucas.alves@engenharia.ufjf.br>
2021-04-12 17:07:01 -0300

    Create user profile retrieval structure

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from "multer";
 
 import uploadConfig from "@config/upload";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { ProfileUserController } from "@modules/accounts/useCases/profileUser/ProfileUserController";
 import { UpdateuserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
@@ -13,6 +14,7 @@ const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateuserAvatarController = new UpdateuserAvatarController();
+const profileUserController = new ProfileUserController();
 
 usersRoutes.post("/", createUserController.handle);
 usersRoutes.patch(
@@ -21,5 +23,6 @@ usersRoutes.patch(
   uploadAvatar.single("avatar"),
   updateuserAvatarController.handle
 );
+usersRoutes.get("/profile", ensureAuthenticated, profileUserController.handle);
 
 export { usersRoutes };
